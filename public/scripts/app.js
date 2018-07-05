e24ab69280242caa6564f281718f00aae9a29c4e
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-05 19:57:38 -0300

    Adicionando render ao clicar de qualquer botao Criando funcao para renderizacao sempre que a funcao for chamada

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,47 +51,52 @@ var template = React.createElement(
 var count = 0;
 
 var addOne = function addOne() {
-	console.log('addOne');
-};
-
-var minusOne = function minusOne() {
-	console.log('minusOne');
-};
-
-var reset = function reset() {
-	console.log('reset');
+	count++;
+	renderCounterApp();
 };
 
 // Challenge
+var minusOne = function minusOne() {
+	count--;
+	renderCounterApp();
+};
 
+var reset = function reset() {
+	count = 0;
+	renderCounterApp();
+};
 // -- Challenge
 
-var templateTwo = React.createElement(
-	'div',
-	null,
-	React.createElement(
-		'h1',
-		null,
-		'Count: ',
-		count
-	),
-	React.createElement(
-		'button',
-		{ onClick: addOne },
-		'+1'
-	),
-	React.createElement(
-		'button',
-		{ onClick: minusOne },
-		'-1'
-	),
-	React.createElement(
-		'button',
-		{ onClick: reset },
-		'reset'
-	)
-);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+	var templateTwo = React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'h1',
+			null,
+			'Count: ',
+			count
+		),
+		React.createElement(
+			'button',
+			{ onClick: addOne },
+			'+1'
+		),
+		React.createElement(
+			'button',
+			{ onClick: minusOne },
+			'-1'
+		),
+		React.createElement(
+			'button',
+			{ onClick: reset },
+			'reset'
+		)
+	);
+	ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -7,48 +7,55 @@ const app = {
 };
 
 const template = (
-<div>	
+	<div>	
 	<h1>{app.title}</h1>
 	{app.subtitle && <p>{app.subtitle}</p>}
 	{(app.options && app.options.length > 0) ? <p>Here are your options</p> : <p>No Options</p>}
 	<ol>
-		<li>Item one</li>
-		<li>Item two</li>
+	<li>Item one</li>
+	<li>Item two</li>
 	</ol>
-</div>
-);
+	</div>
+	);
 
 // class -> className
 
 let count = 0;
 
 const addOne = () => {
-	console.log('addOne');
-};
-
-const minusOne = () => {
-	console.log('minusOne');
-};
-
-const reset = () => {
-	console.log('reset');
+	count++;
+	renderCounterApp();
 };
 
 // Challenge
+const minusOne = () => {
+	count--;
+	renderCounterApp();
+};
 
+const reset = () => {
+	count = 0;
+	renderCounterApp();
+};
 // -- Challenge
 
-const templateTwo = (
-	<div>
-		<h1>Count: {count}</h1>
-		<button onClick={addOne}>+1</button>
-		<button onClick={minusOne}>-1</button>
-		<button onClick={reset}>reset</button>
-	</div>
-);
+
 
 
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+
+const renderCounterApp = () => {
+	const templateTwo = (
+		<div>
+		<h1>Count: {count}</h1>
+		<button onClick={addOne}>+1</button>
+		<button onClick={minusOne}>-1</button>
+		<button onClick={reset}>reset</button>
+		</div>
+		);
+	ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
