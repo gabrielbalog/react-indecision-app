403c26fea1d44349d6a93ed472662aec3bb0a0ca
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-07 00:33:11 -0300

    Adicionando novas classes que fazem inherit de outra classe Adicionando novos campos ao constructor Usando super no constructor Fazendo o super em funcoes da classe pai

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -5,17 +5,59 @@ class Person {
 	}
 
 	getGreetings() {
-		return `Hi. I am ${ this.name }!`;
+		return `Hi. I am ${this.name}!`;
 	}
 
 	getDescription() {
-		return `${ this.name } is ${ this.age } year(s) old.`;
+		return `${this.name} is ${this.age} year(s) old.`;
 	}
 }
 
-const me = new Person('Gabriel Balog', 21);
-console.log(me.getDescription());
+class Student extends Person {
+	constructor(name,age,major) {
+		super(name,age);
+		this.major = major
+	}
 
-const other = new Person();
+	hasMajor() {
+		return !!this.major;
+	}
+
+	getDescription() {
+		let description = super.getDescription();
+
+		if (this.hasMajor()) {
+			description += ` Their Major is ${this.major}`
+		}
+
+		return description;
+	}
+}
+
+class Traveler extends Person {
+	constructor(name,age,homeLocation) {
+		super(name,age);
+		this.homeLocation = homeLocation;
+	}
+
+	hasLocation() {
+		return !!this.homeLocation;
+	}
+
+	getGreetings() {
+		let greetings = super.getGreetings();
+
+		if (this.hasLocation()) {
+			greetings += ` I'm visiting from ${this.homeLocation}`;
+		}
+
+		return greetings;
+	}
+}
+
+const me = new Traveler('Gabriel Balog', 21, 'São Pauo');
+console.log(me.getGreetings());
+
+const other = new Traveler();
 console.log(other.getDescription());
 
