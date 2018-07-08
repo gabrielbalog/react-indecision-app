4848a369341f67858701c6c4b13b858caa55eec9
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-08 19:22:43 -0300

    Adicionando os arquivos de utils e person Exportando e importando funcoes de outros arquivos

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,36 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('app is running!')
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
+// // import './utils.js'
+// import { square, add } from './utils.js'
+
+// console.log('app is running!')
+
+// console.log(square(4))
+// console.log(add(1,1))
+
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](40));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](29));
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const isAdult = (age) => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+
+const canDrink = (age) => age >= 21;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('app is running!')
+// // import './utils.js'
+// import { square, add } from './utils.js'
+
+// console.log('app is running!')
+
+// console.log(square(4))
+// console.log(add(1,1))
+
+import { isAdult, canDrink } from './person.js'
+
+console.log(isAdult(40));
+console.log(canDrink(29));
