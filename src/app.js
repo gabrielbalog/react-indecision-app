38b5de403621cdf4576dae6379e14c01a0e439b9
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-08 13:42:45 -0300

    Ajustando return de funcoes que podem ser retornado em shorthand Criando um novo metodo handleDeleteOption, para deletar uma unica opcao Este metodo e passado em dois componentes, necessario maior logica para ele funcionar

--- src/app.js
+++ src/app.js
@@ -4,17 +4,23 @@ class IndecisionApp extends React.Component {
 		this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
 		this.handlePick = this.handlePick.bind(this)
 		this.handleAddOption = this.handleAddOption.bind(this)
+		this.handleDeleteOption = this.handleDeleteOption.bind(this)
 		this.state = {
 			options: props.options
 		}
 	}
 
 	handleDeleteOptions() {
-		this.setState(() => {
-			return {
-				options: []
-			}
-		})
+		// ({}) significa retorna um objeto
+		this.setState(() => ({ options: [] }));
+	}
+
+	handleDeleteOption(optionToRemove) {
+		this.setState((prevState) => ({
+			options: prevState.options.filter((option) => {
+				return optionToRemove !== option;
+			})
+		}));
 	}
 
 	handlePick() {
@@ -30,11 +36,7 @@ class IndecisionApp extends React.Component {
 			return 'This option already exists';
 		}
 
-		this.setState((prevState) => {
-			return {
-				options: prevState.options.concat([option])
-			}
-		})
+		this.setState((prevState) => ({ options: prevState.options.concat([option]) }))
 	}
 
 	render() {
@@ -50,6 +52,7 @@ class IndecisionApp extends React.Component {
 				<Options 
 					options={this.state.options}
 					handleDeleteOptions={this.handleDeleteOptions}
+					handleDeleteOption={this.handleDeleteOption}
 				/>
 				<AddOption 
 					handleAddOption={this.handleAddOption}
@@ -94,7 +97,15 @@ const Options = (props) => {
 	return (
 		<div>
 			<button onClick={props.handleDeleteOptions}>Remove All</button>
-				{props.options.map((option) => <Option key={option} optionText={option}/>)}
+				{
+					props.options.map((option) => (
+						<Option 
+						key={option} 
+						optionText={option}
+						handleDeleteOption={props.handleDeleteOption}
+						/>
+					))
+				}
 		</div>
 	);
 }
@@ -103,6 +114,13 @@ const Option = (props) => {
 	return (
 		<div>
 			{props.optionText}
+			<button 
+				onClick={() => {
+					props.handleDeleteOption(props.optionText);
+				}}
+				>
+				remove
+			</button>
 		</div>
 	)
 }
@@ -120,9 +138,7 @@ class AddOption extends React.Component {
 		const option = e.target.elements.option.value.trim();
 		const error = this.props.handleAddOption(option);
 
-		this.setState((prevState) => {
-			return { error };
-		})
+		this.setState((prevState) => ({ error }))
 	}
 	render() {
 		return (
