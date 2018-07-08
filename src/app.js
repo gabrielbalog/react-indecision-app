b2986edebf07e497169c311fe37f3d46569bfb7f
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-08 19:45:10 -0300

    Adicionando a biblioteca de React e ReactDOM Importando bibliotecas e as utilizando

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
-// import './utils.js'
-// defaultFromUtils eh a funcao default do utils pode nomear como quiser
-// import defaultFromUtils, { square, add } from './utils.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app is running!')
+const template = React.createElement('p', {}, 'testing');
 
-// console.log(square(4))
-// console.log(add(1,1))
-// console.log(defaultFromUtils(1,1))
-
-import isSenior, { isAdult, canDrink } from './person.js'
-
-console.log(isAdult(40));
-console.log(canDrink(29));
-console.log(isSenior(70));
+ReactDOM.render(template, document.getElementById('app'));
