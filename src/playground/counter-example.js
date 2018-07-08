163476a395844adbe65df19bf61f68843dce89fa
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-08 14:28:13 -0300

    Removendo defaulProps para IndecisionApp e Counter, pois eles recebem o valor do localstorage Escrevendo as funcoes para o app Counter utilizar LocalStorage

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,29 @@ class Counter extends React.Component {
 		this.handleMinusOne = this.handleMinusOne.bind(this);
 		this.handleReset = this.handleReset.bind(this);
 		this.state = {
-			count: props.count
+			count: 0
+		}
+	}
+
+	componentDidMount() {
+		try {
+			const json = localStorage.getItem('count');
+			const count = JSON.parse(json);
+
+			if (count) {
+				this.setState(() => ({ count }));
+			}
+		} catch(e) {
+			// statements
+			console.log(e);
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.count !== this.state.count) {
+			const json = JSON.stringify(this.state.count);
+			localStorage.setItem('count', json);
+			console.log('x')
 		}
 	}
 
@@ -69,9 +91,6 @@ class Counter extends React.Component {
 	}
 }
 
-Counter.defaultProps = {
-	count: 0
-}
 
 ReactDOM.render(<Counter />,document.getElementById('app'))
 
