b6803798c2cd1ccf50b0e8a9df6cf060b4e19862
Unknown <gabriel.balog@bradootech.com>
2018-07-08 21:31:08 -0300

    Adicionando o pacote react-modal, que traz a opcao de um modal personalizado Adicao de arquivo que contem o codigo do modal Refactor de funcoes para chamar o modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,16 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
 	state = {
-		options: []
+		options: [],
+		selectedOption: undefined
+	}
+
+	handleClearSelectedOption = () => {
+		this.setState(() => ({ selectedOption: undefined }));
 	}
 
 	handleDeleteOptions = () => {
@@ -25,7 +31,7 @@ export default class IndecisionApp extends React.Component {
 	handlePick = () => {
 		const randomNum = Math.floor(Math.random() * this.state.options.length);
 		const option = this.state.options[randomNum];
-		alert(option)
+		this.setState(() => ({ selectedOption: option }));
 	}
 
 	handleAddOption = (option) => {
@@ -85,6 +91,10 @@ export default class IndecisionApp extends React.Component {
 				<AddOption 
 					handleAddOption={this.handleAddOption}
 				/>
+				<OptionModal
+				selectedOption={this.state.selectedOption}
+				handleClearSelectedOption={this.handleClearSelectedOption}
+				/> 
 			</div>
 		)
 	}
